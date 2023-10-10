38a31f358690807147e5fe3b00cde3baab4b4c9d
Wanzetse Maloba <wanzetseofficial@gmail.com>
2023-10-10 23:19:23 +0300

    fix: change mongodb-connection to container name

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://moringa-devops-04-ip2-mongo/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
